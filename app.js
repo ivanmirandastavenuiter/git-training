202700a3fc521872c89d68f11fa4c07bce3787e9
ivanmirandastavenuiter <ivanmist90@gmail.com>
2021-04-28 15:45:33 +0200

    Second cool commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const { json } = require('express');
 const express = require('express');
 const app = express();
+app.use(json());
 
 app.get('/git-training', (req, res) => {
     res.send( `Welcome to git training on Node.js`);
