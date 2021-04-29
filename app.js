3c937b9cd85c1ad1870f45f54cc36944547c1c6b
ivanmirandastavenuiter <ivanmist90@gmail.com>
2021-04-29 11:58:23 +0200

    About to test git reset over commits

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ import express from 'express';
 const app = express().use(express.json());
 
 app.get('/git-training', (req, res) => {
+    const headers = req.headers;
     res.send({ res: `Welcome to git training on Node.jds` });
 });
+
+console.log('1');
+console.log('2');
 
 app.listen(3000);
