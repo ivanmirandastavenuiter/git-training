1d03833a08aa168e606a11c39e57379af3091933
ivanmirandastavenuiter <ivanmist90@gmail.com>
2021-04-29 12:34:52 +0200

    Committing after resetting through soft git reset switch

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ import express from 'express';
 const app = express().use(express.json());
 
 app.get('/git-training', (req, res) => {
-    const headers = req.headers;
     res.send({ res: `Welcome to git training on Node.jds` });
 });
 
-console.log('1');
-console.log('2');
+// Testing git reset with commit hash
 
 app.listen(3000);
